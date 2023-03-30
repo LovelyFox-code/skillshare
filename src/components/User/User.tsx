d7dbcd5f763da1ca17ddbc3c822e4c7f973bacfc
Alina <alinadakhno60@gmail.com>
2023-03-30 11:28:39 +0200

    fetch random user to display

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import styles from "./User.module.css";
 import { HiOutlineUserCircle } from "react-icons/hi";
 
 const User = () => {
+  const [user, setUser]= useState({
+    name: "Name",
+    picture: "logo"
+
+  });
+   useEffect(()=>{
+    const userFetch =async () => {
+      const user = await(
+        await fetch(
+          "https://randomuser.me/api"
+        )
+      ).json();
+      setUser({...user, name: user.results[0].name.first, picture: user.results[0].picture.large})
+    }
+    userFetch()
+  }, [])
   return (
     <div className={styles.wrapper}>
       <div className={styles.user}>
-        <div className={styles.user_img}><HiOutlineUserCircle size="100px" color="#0b9dcf" /></div>
-        <h1>Name </h1>
+        {user.picture ? <img src={user.picture}/> : <div className={styles.user_img}><HiOutlineUserCircle size="100px" color="#0b9dcf" /></div>}
+        {user.name ? <h1>{user.name}</h1> : <form>
+      <label>Enter your name:
+        <input type="text" />
+      </label>
+    </form>}
         <p>front-end</p>
       </div>
     </div>
